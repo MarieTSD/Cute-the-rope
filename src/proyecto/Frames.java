007c411987e824302ef80fcd4aa4189f5e753662
marie <marie.serna11@gmail.com>
2021-01-10 22:50:54 -0600

    fin

---
+++ src/proyecto/Frames.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto;
+
+/**
+ *
+ * @author marie
+ */
+public class Frames {
+    inicio1 ini = new inicio1();
+    instrucciones ins = new instrucciones();
+    
+}
---
+++ src/proyecto/PROYECTO.java
@@ -0,0 +1,22 @@
+
+package proyecto;
+
+/**
+ *
+ * @author marie
+ */
+
+public class PROYECTO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Frames Control = new Frames();
+        Control.ini.setVisible(true);
+       
+        
+        // TODO code application logic here
+    }
+    
+}
